30ba66dbf8ab275d273be120cbefc8ff7089b44e
Queven <trabalhoqueven@gmail.com>
2026-03-04 16:09:06 -0300

    Refactor firebase.js to remove auth initialization
    Removed authentication initialization and export.

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -12,5 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
