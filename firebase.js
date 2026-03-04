bc6d3fd17798c2b8505ad0d8753beb044930b895
Queven <trabalhoqueven@gmail.com>
2026-03-04 10:41:51 -0300

    Add storage bucket and messaging sender ID to config

--- firebase.js
+++ firebase.js
@@ -6,6 +6,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyD4IahzztFjZKZm4D4qpXuSEJqa_4A3bNU",
   authDomain: "qmap-2026.firebaseapp.com",
   projectId: "qmap-2026",
+  storageBucket: "qmap-2026.firebasestorage.app",
+  messagingSenderId: "257863222905",
+  appId: "1:257863222905:web:ed20ebe061bbbe382e76a8"
 };
 
 const app = initializeApp(firebaseConfig);
